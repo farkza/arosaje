a7b2223cc0f7739bf6f0a1ed984fa7fdce80f96f
farkza <farkzapro@gmail.com>
2024-02-13 23:59:20 +0100

    update files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React from 'react';
+import AppRouter from './AppRouter'; // Assurez-vous que le chemin est correct
 
 function App() {
-  const [plantes, setPlantes] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:8000/api/plantes')
-      .then(response => response.json())
-      .then(data => setPlantes(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
-
   return (
-    <div className="App">
-      <h1>Liste des plantes</h1>
-      <ul>
-        {plantes.map(plante => (
-          <li key={plante.id}>
-            <strong>Nom:</strong> {plante.nom}, <strong>Poids:</strong> {plante.poids}, <strong>Espèce:</strong> {plante.especes}
-          </li>
-        ))}
-      </ul>
+    <div>
+      <AppRouter />
     </div>
   );
 }
