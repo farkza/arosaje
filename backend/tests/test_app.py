89aa534f6b93bbe8d519958fd23c1091fa8484e5
Julie Montoux <92477225+JulieMontoux@users.noreply.github.com>
2024-02-18 12:09:55 +0000

    update directory

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,17 +1,25 @@
-# from fastapi.testclient import TestClient
-# from backend.main import app
+from fastapi.testclient import TestClient
+from main import app
+import pytest
 
-# client = TestClient(app)
+TEST_DATABASE_PATH = "test_arosaje.db"
 
-# def test_read_root():
-#    response = client.get("/")
-#    assert response.status_code == 200
-#    assert response.json() == {"message": "Bienvenue sur mon API FastAPI"}
+@pytest.fixture(scope="module")
+def test_app():
+    client = TestClient(app)
+    yield client
 
-def add(a, b):
-    return a + b
+def test_read_root(test_app):
+    response = test_app.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Bienvenue sur mon API FastAPI"}
 
-def test_addition():
-    assert add(1, 2) == 3
-    assert add(0, 0) == 0
-    assert add(-1, 1) == 0
+def test_get_plantes_from_db(test_app):
+    response = test_app.get("/get_plantes_from_db")
+    assert response.status_code == 200
+    assert response.json().get("plantes") is not None
+
+def test_test_db_connection(test_app):
+    response = test_app.get("/test_db_connection")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Connexion à la base de données réussie !"}
